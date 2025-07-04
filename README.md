9df5cb45edb4210ace6e50e7341f8a730e9d397d
Vishesh12Dubey <visheshdubey647@gmail.com>
2025-07-05 00:12:07 +0530

    add

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Mahakal
+This is my first Git Repository.
+<br>
+Author - Vishesh Dubey(VD).
---
+++ note.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    system("cls");
+    
+    cout<< "Ramayan";
+    cout<< "Gita";
+    getchar();
+    return 0;
+}
